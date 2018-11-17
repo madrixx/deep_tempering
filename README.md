f4adec654102ee03ad01573685c946655d6a144f
vlad-user <vladpush@campus.technion.ac.il>
2018-11-17 22:16:00 +0200

    small changes

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 from tensorflow.examples.tutorials.mnist import input_data
 
 from simulation.simulator import Simulator
-from simulation.architectures.mnist_architectures import nn_mnist_architecture
+from simulation.models.mnist_models import nn_mnist_model
 from simulation.summary_extractor2 import SummaryExtractor
 import simulation.simulator_utils as s_utils
 
@@ -27,11 +27,11 @@ batch_size = 50
 n_epochs = 5
 learning_rate = 0.01
 beta_0 = 200
-temp_factor = 1.1
+separation_ratio = 1.1
 n_replicas = 8
-noise_list = [beta_0*temp_factor**i for i in range(n_replicas)]
+noise_list = [beta_0*separation_ratio**i for i in range(n_replicas)]
 name = 'ptld_simulation'
-swap_attempt_step = 500 # 1 step==batch_size
+swap_step = 500 # 1 step==batch_size
 burn_in_period = 2000
 test_step = 500
 description='Parallel Tempering with Langevin Dynamics'
@@ -43,24 +43,24 @@ s_utils.clean_dirs('simulation/summaries/compressed/' + name)
 
 # create and run simulation
 sim = Simulator(
-	architecture=nn_mnist_architecture,
-	learning_rate=learning_rate,
-	noise_list=noise_list,
-	noise_type='betas',
-	batch_size=batch_size,
-	n_epochs=n_epochs,
-	name=name,
-	swap_attempt_step=swap_attempt_step,
-	test_step=test_step,
-	temp_factor=temp_factor,
-	description=description,
-	burn_in_period=burn_in_period,
-	loss_func_name='cross_entropy'
-	)
+  model=nn_mnist_model,
+  learning_rate=learning_rate,
+  noise_list=noise_list,
+  noise_type='betas',
+  batch_size=batch_size,
+  n_epochs=n_epochs,
+  name=name,
+  swap_step=swap_step,
+  test_step=test_step,
+  separation_ratio=separation_ratio,
+  description=description,
+  burn_in_period=burn_in_period,
+  loss_func_name='cross_entropy'
+  )
 
 sim.train(train_data=train_data, train_labels=train_labels,
-	test_data=test_data, test_labels=test_labels, 
-	validation_data=valid_data, validation_labels=valid_labels)
+  test_data=test_data, test_labels=test_labels, 
+  validation_data=valid_data, validation_labels=valid_labels)
 
 
 # plot results
@@ -77,7 +77,7 @@ import numpy as np
 from simulation.simulator import Simulator
 from simulation.summary_extractor2 import SummaryExtractor
 import simulation.simulator_utils as s_utils
-from simulation.architectures.mnist_architectures import nn_mnist_architecture_dropout
+from simulation.models.mnist_models import nn_mnist_model_dropout
 
 MNIST_DATAPATH = 'simulation/data/mnist/'
 
@@ -92,7 +92,7 @@ valid_labels = mnist.validation.labels
 n_replicas = 8
 
 # set simulation parameters
-architecture_func = nn_mnist_architecture_dropout
+model_func = nn_mnist_model_dropout
 learning_rate = 0.01
 noise_list = [1-x for x in np.geomspace(start=0.01, stop=0.99, num=n_replicas)]
 noise_type = 'dropout_rmsprop'
@@ -100,7 +100,7 @@ batch_size = 200
 n_epochs = 20
 name = 'rmsprop_simulation' # simulation name
 test_step = 400 # 1 step==batch_size
-swap_attempt_step = 300
+swap_step = 300
 burn_in_period = 400
 loss_func_name = 'cross_entropy'
 description = 'RMSProp with dropout.'
@@ -117,27 +117,27 @@ s_utils.clean_dirs('simulation/summaries/compressed/' + name)
 # create and run simulation
 
 sim = Simulator(
-	architecture=architecture_func,
-	learning_rate=learning_rate,
-	noise_list=noise_list,
-	noise_type='dropout_rmsprop',
-	batch_size=batch_size,
-	n_epochs=n_epochs,
-	test_step=test_step,
-	name=name,
-	swap_attempt_step=swap_attempt_step,
-	burn_in_period=burn_in_period,
-	loss_func_name='cross_entropy',
-	description=description,
-	proba_coeff=proba_coeff,
-	rmsprop_decay=rmsprop_decay,
-	rmsprop_epsilon=rmsprop_epsilon,
-	rmsprop_momentum=rmsprop_momentum
-	)
+  model=model_func,
+  learning_rate=learning_rate,
+  noise_list=noise_list,
+  noise_type='dropout_rmsprop',
+  batch_size=batch_size,
+  n_epochs=n_epochs,
+  test_step=test_step,
+  name=name,
+  swap_step=swap_step,
+  burn_in_period=burn_in_period,
+  loss_func_name='cross_entropy',
+  description=description,
+  proba_coeff=proba_coeff,
+  rmsprop_decay=rmsprop_decay,
+  rmsprop_epsilon=rmsprop_epsilon,
+  rmsprop_momentum=rmsprop_momentum
+  )
 
 sim.train(train_data=train_data, train_labels=train_labels,
-	test_data=test_data, test_labels=test_labels, 
-	validation_data=valid_data, validation_labels=valid_labels)
+  test_data=test_data, test_labels=test_labels, 
+  validation_data=valid_data, validation_labels=valid_labels)
 
 
 # plot results
--- simulation/multi_experiment_simulator.py
+++ simulation/multi_experiment_simulator.py
@@ -3,147 +3,145 @@ from time import time
 from simulation.simulator_utils import generate_experiment_name
 from simulation.simulator import Simulator
 
-class MultiExperimentSimulator(object):
-	def __init__(self, architecture, tuning_parameter_vals, n_replicas, beta_0,
-		learning_rate, architecture_name, surface_view, data, noise_type,
-		n_simulations=10, batch_size=50, n_epochs=25, swap_attempt_step=400, 
-		dataset_name='mnist', tuning_param_name='tempfactor', swap_proba='boltzmann',
-		optimizer_name='PTLD', loss_func_name='crossentropy', do_swaps=True, 
-		start_experiments_at=0, burn_in_period=None,
-		temp_factor=None, description='v3', verbose=True):
+class MultiExperimentSimulator: # pylint: disable=too-many-instance-attributes, too-few-public-methods
+  """Executes multiple experiments."""
+  def __init__(# pylint: disable=too-many-locals, too-many-arguments
+      self, model, tuning_parameter_vals, n_replicas, beta_0,
+      learning_rate, model_name, surface_view, data, noise_type,
+      n_simulations=10, batch_size=50, n_epochs=25, swap_step=400,
+      dataset_name='mnist', loss_func_name='crossentropy', do_swaps=True,
+      start_experiments_at=0, burn_in_period=None,
+      separation_ratio=None, description='v7', verbose=True):
 
-		self.n_experiments = len(tuning_parameter_vals)
+    self.n_experiments = len(tuning_parameter_vals)
 
-		self.experiment_name = generate_experiment_name(
-			architecture_name=architecture_name, dataset=dataset_name, 
-			temp_ratio=tuning_param_name.replace('_', ''), optimizer=optimizer_name,
-			do_swaps=do_swaps, swap_proba='boltzmann', n_replicas=n_replicas,
-			surface_view=surface_view, beta_0=beta_0, 
-			loss_func_name=loss_func_name.replace('_',''), 
-			swap_attempt_step=swap_attempt_step, burn_in_period=burn_in_period, 
-			learning_rate=learning_rate, n_epochs=n_epochs)
+    self.experiment_name = generate_experiment_name(
+        model_name=model_name, dataset=dataset_name,
+        separation_ratio=separation_ratio,
+        do_swaps=do_swaps, n_replicas=n_replicas,
+        beta_0=beta_0, loss_func_name=loss_func_name.replace('_', ''),
+        swap_step=swap_step, burn_in_period=burn_in_period,
+        learning_rate=learning_rate, n_epochs=n_epochs)
 
-		self.architecture = architecture
-		self.tuning_parameter_vals = tuning_parameter_vals
-		self.n_replicas = n_replicas
-		self.beta_0 = beta_0
-		self.learning_rate = learning_rate
-		self.architecture_name = architecture_name
-		self.surface_view = surface_view
-		self.n_simulations = n_simulations
-		self.batch_size = batch_size
-		self.n_epochs = n_epochs
-		self.swap_attempt_step = swap_attempt_step
-		self.dataset_name = dataset_name
-		self.tuning_param_name = tuning_param_name
-		self.swap_proba = swap_proba
-		self.optimizer_name = optimizer_name
-		self.loss_func_name = loss_func_name
-		self.do_swaps = do_swaps,
-		self.description = description
-		self.verbose = verbose
-		self.data = data
-		self.noise_type = noise_type
-		self.start_experiments_at = start_experiments_at
-		self.burn_in_period = burn_in_period
-		self.loss_func_name = loss_func_name
-		self.temp_factor = temp_factor
+    self.model = model
+    self.tuning_parameter_vals = tuning_parameter_vals
+    self.n_replicas = n_replicas
+    self.beta_0 = beta_0
+    self.learning_rate = learning_rate
+    self.model_name = model_name
+    self.surface_view = surface_view
+    self.n_simulations = n_simulations
+    self.batch_size = batch_size
+    self.n_epochs = n_epochs
+    self.swap_step = swap_step
+    self.dataset_name = dataset_name
+    self.loss_func_name = loss_func_name
+    self.do_swaps = do_swaps
+    self.description = description
+    self.verbose = verbose
+    self.data = data
+    self.noise_type = noise_type
+    self.start_experiments_at = start_experiments_at
+    self.burn_in_period = burn_in_period
+    self.loss_func_name = loss_func_name
+    self.separation_ratio = separation_ratio
 
 
 
-	def run_all(self, beta_0_list=None):
-		if self.tuning_param_name.replace('_', '') == 'tempfactor':
-			self._run_tempfactor_experiments()
-		elif self.tuning_param_name == 'beta0':
-			self._run_accept_experiments(beta_0_list)
+  def run_all(self, beta_0_list=None):
+    if self.tuning_param_name.replace('_', '') == 'tempfactor':
+      self._run_tempfactor_experiments()
+    elif self.tuning_param_name == 'beta0':
+      self._run_accept_experiments(beta_0_list)
 
-	def _run_accept_experiments(self, beta0_list):
-		separation_ratio = self.temp_factor
-		if beta0_list is None:
-			raise ValueError("beta0_list is None, but must contain beta_0 vals.")
+  def _run_accept_experiments(self, beta0_list):
+    separation_ratio = self.separation_ratio
+    if beta0_list is None:
+      raise ValueError("beta0_list is None, but must contain beta_0 vals.")
 
-		timer = Timer()
-		name = self.experiment_name
-		
-		for exp in range(self.start_experiments_at, len(beta0_list)):
-			beta0 = beta0_list[exp]
-			noise_list = [beta0, beta0*separation_ratio]
-			name = self.experiment_name + '_' + str(exp)
-			if self.verbose: print('experiment:', exp+1, '/', len(beta0_list), 
-				', separation_ratio:', separation_ratio)
-			if self.verbose: print(noise_list)
+    timer = Timer()
+    name = self.experiment_name
 
-			sim = Simulator(self.architecture, self.learning_rate,
-				noise_list, self.noise_type, batch_size=self.batch_size,
-				n_epochs=self.n_epochs, name=name, n_simulations=self.n_simulations,
-				swap_attempt_step=self.swap_attempt_step,
-				temp_factor=separation_ratio, 
-				tuning_parameter_name=self.tuning_param_name,
-				surface_view=self.surface_view, burn_in_period=self.burn_in_period,
-				loss_func_name=self.loss_func_name, description=self.description)
+    for exp in range(self.start_experiments_at, len(beta0_list)):
+      beta0 = beta0_list[exp]
+      noise_list = [beta0, beta0*separation_ratio]
+      name = self.experiment_name + '_' + str(exp)
+      if self.verbose:
+        print(
+            'experiment:', exp+1, '/', len(beta0_list),
+            ', separation_ratio:', separation_ratio)
+      if self.verbose:
+        print(noise_list)
 
-			sim.train_n_times(sim.train_PTLD, train_data=self.data['train_data'],
-				train_labels=self.data['train_labels'], 
-				test_data=self.data['test_data'],
-				test_labels=self.data['test_labels'], 
-				validation_data=self.data['valid_data'], 
-				validation_labels=self.data['valid_labels']
-				)
-			if self.verbose: print()
-			if self.verbose: print('time took:', timer.elapsed_time(), 'min')
+      sim = Simulator(
+          self.model, self.learning_rate,
+          noise_list, self.noise_type, batch_size=self.batch_size,
+          n_epochs=self.n_epochs, name=name, n_simulations=self.n_simulations,
+          swap_step=self.swap_step,
+          separation_ratio=separation_ratio,
+          tuning_parameter_name=self.tuning_param_name,
+          surface_view=self.surface_view, burn_in_period=self.burn_in_period,
+          loss_func_name=self.loss_func_name, description=self.description)
 
-			#exp += 1
+      sim.train_n_times(
+          sim.train_PTLD, train_data=self.data['train_data'],
+          train_labels=self.data['train_labels'],
+          test_data=self.data['test_data'],
+          test_labels=self.data['test_labels'],
+          validation_data=self.data['valid_data'],
+          validation_labels=self.data['valid_labels'])
+      if self.verbose:
+        print()
+      if self.verbose:
+        print('time took:', timer.elapsed_time(), 'min')
 
+  def _run_tempfactor_experiments(self):
+    timer = Timer()
+    for exp in range(self.start_experiments_at, self.n_experiments):
+      separation_ratio = self.tuning_parameter_vals[exp]
+      name = self.experiment_name + '_' + str(exp)
+      if self.verbose:
+        print(
+            'experiment:', exp+1, '/', self.n_experiments,
+            ', param_val:', separation_ratio)
+      if self.verbose:
+        print(name)
 
-		
-
-	def _run_tempfactor_experiments(self):
-		timer = Timer()
-		for exp in range(self.start_experiments_at, self.n_experiments):
-			temp_factor = self.tuning_parameter_vals[exp]
-			name = self.experiment_name + '_' + str(exp)
-			if self.verbose: print('experiment:', exp+1, '/', self.n_experiments, 
-				', param_val:', temp_factor)
-			if self.verbose: print(name)
-
-			noise_list = [self.beta_0*temp_factor**i for i in range(self.n_replicas)]
-			if self.verbose: print(noise_list)
-
-
-			sim = Simulator(self.architecture, self.learning_rate, 
-				noise_list, self.noise_type,
-				batch_size=self.batch_size, n_epochs=self.n_epochs,
-				name=name, n_simulations=self.n_simulations, 
-				swap_attempt_step=self.swap_attempt_step, 
-				temp_factor=temp_factor,
-				tuning_parameter_name=self.tuning_param_name,
-				surface_view=self.surface_view, burn_in_period=self.burn_in_period,
-				loss_func_name=self.loss_func_name, description=self.description)
-
-			sim.train_n_times(sim.train_PTLD, train_data=self.data['train_data'],
-				train_labels=self.data['train_labels'], 
-				test_data=self.data['test_data'],
-				test_labels=self.data['test_labels'], 
-				validation_data=self.data['valid_data'], 
-				validation_labels=self.data['valid_labels']
-				)
-			if self.verbose: print()
-			if self.verbose: print('time took:', timer.elapsed_time(), 'min')
+      noise_list = [self.beta_0*separation_ratio**i
+                    for i in range(self.n_replicas)]
+      if self.verbose:
+        print(noise_list)
 
 
+      sim = Simulator(
+          self.model, self.learning_rate,
+          noise_list, self.noise_type,
+          batch_size=self.batch_size, n_epochs=self.n_epochs,
+          name=name, n_simulations=self.n_simulations,
+          swap_step=self.swap_step,
+          separation_ratio=separation_ratio,
+          surface_view=self.surface_view, burn_in_period=self.burn_in_period,
+          loss_func_name=self.loss_func_name, description=self.description)
 
+      sim.train_n_times(
+          sim.train_PTLD, train_data=self.data['train_data'],
+          train_labels=self.data['train_labels'],
+          test_data=self.data['test_data'],
+          test_labels=self.data['test_labels'],
+          validation_data=self.data['valid_data'],
+          validation_labels=self.data['valid_labels'])
+      if self.verbose:
+        print()
+      if self.verbose:
+        print('time took:', timer.elapsed_time(), 'min')
 
-
-
-
-
-
-class Timer(object):
-    def __init__(self):
-        self.start_time = time()
-    def start_timer(self):
-        self.start_time = time()
-    def elapsed_time(self):
-        res = int((time() - self.start_time) / 60.0)
-        self.start_time = time()
-        return res
+class Timer:
+  """Helper for measuring simulation time."""
+  def __init__(self):
+    self.start_time = time()
+  def start_timer(self):
+    self.start_time = time()
+  def elapsed_time(self):
+    res = int((time() - self.start_time) / 60.0)
+    self.start_time = time()
+    return res
